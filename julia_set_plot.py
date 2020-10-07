43c9597ff5299f8457fd05596494be106a0ba980
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-10-07 07:17:09 -0800

    CLI args for num_steps and max_iterations.

--- julia_set_plot.py
+++ julia_set_plot.py
@@ -1,22 +1,36 @@
 """Generate an image of the julia set."""
 
+import time, argparse
+
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+# Parse cli arguments.
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--num-steps',
+    help="How many steps between horizontal and vertical bounds?",
+    action='store', type=int, default=200)
+
+parser.add_argument('--max-iterations',
+    help="What's the max number of iterations to try on each point?",
+    action='store', type=int, default=20)
+
+args = parser.parse_args()
 
 
 # Bounds and critical values
 a_bound, b_bound = 1.8, 1.8
 c = -0.62772 - 0.42193j
-max_iterations = 20
 escape_bound = 2
-num_steps = 200
 
 
 def get_iterations(point):
     """How many iterations does it take to exceed the critical value?"""
     iteration = 0
     pv = point**2 + c
-    while iteration < max_iterations:
+    while iteration < args.max_iterations:
         if abs(pv) > escape_bound:
             return iteration
         else:
@@ -28,8 +42,8 @@ def get_iterations(point):
 def get_julia_points():
     """Define the points, and the number of iterations for each point."""
     points, point_iterations = [], []
-    for a in np.linspace(-a_bound, a_bound, num_steps):
-        for b in np.linspace(-b_bound, b_bound, num_steps):
+    for a in np.linspace(-a_bound, a_bound, args.num_steps):
+        for b in np.linspace(-b_bound, b_bound, args.num_steps):
             point = complex(a, b)
             iterations = get_iterations(point)
             points.append(point)
@@ -51,9 +65,27 @@ def plot_julia_points(points, point_iterations):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-    plt.show()
+    filename = f"output/julia_plot_mpl-{args.num_steps}-maxiter-{args.max_iterations}.png"
+    plt.savefig(filename)
 
 
 if __name__ == '__main__':
+    start = time.perf_counter()
+
     points, point_iterations = get_julia_points()
+    generated_points = time.perf_counter()
+
     plot_julia_points(points, point_iterations)
+    generated_plot = time.perf_counter()
+
+    # Summarize execution time:
+    analysis_time = round(generated_points - start, 1)
+    plotting_time = round(generated_plot - generated_points, 1)
+    total_time = round(generated_plot - start, 1)
+
+    print(f"num_steps: {args.num_steps}")
+    print(f"max_iterations: {args.max_iterations}")
+
+    print(f"\nSpent {analysis_time} seconds generating julia points.")
+    print(f"Spent {plotting_time} seconds generating plot.")
+    print(f"Spent {total_time} seconds in total.")
